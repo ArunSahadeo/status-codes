f106b5f9287e9b50c5d4b2fcc6eed071a4ec010f
Arun Sahadeo <arun@wirebox.co.uk>
2018-05-31 11:32:34 +0100

    Make a Rack based application, add routes to list all 1xx, 2xx, 3xx, 4xx and 5xx routes

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-require "bundler/setup"
 require "json"
 require "sinatra"
 
@@ -32,4 +31,55 @@ not_found do
 	status 404
 	content_type :json
 	{ error: "An invalid route was specified" }.to_json
+end
+
+# Get all routes
+routesAll = []
+Sinatra::Application.routes["GET"].each do |route|
+	routesAll.push(route[0].to_s)
+end
+
+# Get all 1xx routes
+get "/1xx" do
+	content_type :json
+	routes1xx = routesAll.select do |route|
+		route[1] == "1"
+	end
+	{ routes: "#{routes1xx}" }.to_json
+end
+
+# Get all 2xx routes
+get "/2xx" do
+	content_type :json
+	routes2xx = routesAll.select do |route|
+		route[1] == "2"
+	end
+	{ routes: "#{routes2xx}" }.to_json
+end
+
+# Get all 3xx routes
+get "/3xx" do
+	content_type :json
+	routes3xx = routesAll.select do |route|
+		route[1] == "3"
+	end
+	{ routes: "#{routes3xx}" }.to_json
+end
+
+# Get all 4xx routes
+get "/4xx" do
+	content_type :json
+	routes4xx = routesAll.select do |route|
+		route[1] == "4"
+	end
+	{ routes: "#{routes4xx}" }.to_json
+end
+
+# Get all 5xx routes
+get "/5xx" do
+	content_type :json
+	routes5xx = routesAll.select do |route|
+		route[1] == "5"
+	end
+	{ routes: "#{routes5xx}" }.to_json
 end
